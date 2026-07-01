5c7ff53ea393d4f0fcc83e093c04671e27bec5ea
Stefano <stefano.41.dev@gmail.com>
2026-06-30 21:15:30 -0500

    first commit

---
+++ src/test/java/com/stefano/dev/ArqHexagonalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stefano.dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArqHexagonalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
